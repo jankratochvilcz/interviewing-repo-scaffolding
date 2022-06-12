588da50df20f34366c78a4645bb91ba062fd9082
Jan Kratochvil <kratochvil.jan@outlook.com>
2022-06-12 16:04:05 +0200

    Adds more configuration + test

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,17 +1,17 @@
 import { GitHubConfiguration } from "./github";
 
-export const getGitHubConfiguration = (): GitHubConfiguration =>  ({
-    defaultBranch: getRequiredEnvironmentVariable("DEFAULT_BRANCH"),
-    organization: getRequiredEnvironmentVariable("ORGANIZATION_NAME"),
-    token: getRequiredEnvironmentVariable("GITHUB_TOKEN")
-})
+export const getGitHubConfiguration = (): GitHubConfiguration => ({
+  defaultBranch: getRequiredEnvironmentVariable("DEFAULT_BRANCH"),
+  organization: getRequiredEnvironmentVariable("ORGANIZATION_NAME"),
+  token: getRequiredEnvironmentVariable("GITHUB_TOKEN"),
+});
 
 const getRequiredEnvironmentVariable = (name: string): string => {
-    const value = process.env[name]
+  const value = process.env[name];
 
-    if(!value) {
-        throw new Error(`Environment variable ${name} cannot be empty.`);
-    }
+  if (!value) {
+    throw new Error(`Environment variable ${name} cannot be empty.`);
+  }
 
-    return value
-}
+  return value;
+};
